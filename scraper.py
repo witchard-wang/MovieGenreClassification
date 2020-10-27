8c39891adc38ace051b7cd83b33e46f544a3c0ed
Richard Wang <41605756+witchard-wang@users.noreply.github.com>
2020-10-27 13:17:10 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,2 @@
-tmdb = '9bc7e5ee4b6c54454c8b6f0fadf4a1cf'
+tmdbv3 = '9bc7e5ee4b6c54454c8b6f0fadf4a1cf'
+tmdbv4 = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmM3ZTVlZTRiNmM1NDQ1NGM4YjZmMGZhZGY0YTFjZiIsInN1YiI6IjVmOTYzMTQxODhiMTQ4MDAzNjQ0OGQwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nycR-lmbBjeFTabTI4aEa3-cSpQf83BgwW1ZrJqRWmQ'
