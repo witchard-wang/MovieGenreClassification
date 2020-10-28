eb0e3aa33efdee14c831c3a737038290933d470e
Richard Wang <richardwang981@yahoo.com>
2020-10-28 17:57:56 -0500

    get 1 movie

--- scraper.py
+++ scraper.py
@@ -3,9 +3,16 @@ import pandas as pd
 import tmdbsimple as tmdb
 
 tmdbv3 = '9bc7e5ee4b6c54454c8b6f0fadf4a1cf'
-tmdbv4 = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmM3ZTVlZTRiNmM1NDQ1NGM4YjZmMGZhZGY0YTFjZiIsInN1YiI6IjVmOTYzMTQxODhiMTQ4MDAzNjQ0OGQwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nycR-lmbBjeFTabTI4aEa3-cSpQf83BgwW1ZrJqRWmQ'
 
 tmdb.API_KEY = tmdbv3
-movie = tmdb.Movies(603)
+movie = tmdb.Movies(123452)
 response = movie.info()
-print(movie.title)
+
+for i in response.keys():
+    print(i)
+
+print(response['title'])
+print(response['poster_path'])
+print(response['genres'])
+image_path = 'https://image.tmdb.org/t/p/original' + response['poster_path']
+print(image_path)
