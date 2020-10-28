4555ee556efe6e94067e96dab68232a7e234567d
Richard Wang <richardwang981@yahoo.com>
2020-10-28 17:25:35 -0500

    tmdb wrapper instruction

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Classification of Movie Genre based on Movie Poster
 
 This project exists in order to find if there are any dominant features found within movie posters that can be used to classify the movie genre.
+
+Movie data obtained using The Movie Database(TMDB) API 
+
+In order to run scraper must add tmdbsimple to python environment
+`pip install tmdbsimple`
--- scraper.py
+++ scraper.py
@@ -1,2 +1,11 @@
+import requests
+import pandas as pd
+import tmdbsimple as tmdb
+
 tmdbv3 = '9bc7e5ee4b6c54454c8b6f0fadf4a1cf'
 tmdbv4 = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmM3ZTVlZTRiNmM1NDQ1NGM4YjZmMGZhZGY0YTFjZiIsInN1YiI6IjVmOTYzMTQxODhiMTQ4MDAzNjQ0OGQwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nycR-lmbBjeFTabTI4aEa3-cSpQf83BgwW1ZrJqRWmQ'
+
+tmdb.API_KEY = tmdbv3
+movie = tmdb.Movies(603)
+response = movie.info()
+print(movie.title)
