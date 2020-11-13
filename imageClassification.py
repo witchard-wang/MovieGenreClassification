19cf9c40e7650112ef4e38c1a205857a142e11f0
vidhur-potluri <vidhur.potluri@tamu.edu>
2020-11-12 22:28:52 -0600

    Created the neural network
    Takes very long to fit the model

--- imageClassification.py
+++ imageClassification.py
@@ -6,12 +6,16 @@ import pandas as pd
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization, Conv2D, MaxPool2D
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+print(tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
+#Folder specification
+folder = "posters"
 
 # formatting image dimensions
 img_width = 350
 img_height = 350
 img_count = len([item for item in os.listdir('posters') if os.path.isfile(os.path.join('posters', item))])
-print(img_count)
 
 # Formats each image present in folder, and adds them to an array.
 def processImages(folder):
@@ -32,27 +36,115 @@ def processImages(folder):
 #Creates CNN to classify images
 def setupNeuralNetwork():
     data = pd.read_csv("mov_IDs.csv")
-    i = -1
+    i = -1  
     for genres in data['Genres']:
         i = i + 1
         genres = genres.split()
         for genre in genres:
             if genre not in data.columns:
-                data[genre] = [0 for i in range(0,img_count)]
+                data[genre] = [0 for a in range(0,img_count)]
             data.iloc[i, data.columns.get_loc(genre)] = 1
     #print(data)
 
-    dataModel = data.drop(['Title', 'Genres', 'Poster_Path'], axis = 1)
-    dataModel = dataModel.to_numpy()
+    dataModel = data.drop(['Title', 'Genres', 'Poster_Path', 'Release_Date', 'Language'], axis = 1)
     #print(dataModel)
+    dataModel = dataModel.to_numpy()
+    img_array = processImages(folder)
 
-    # training and testing model
-    X_train, X_test, y_train, y_test = train_test_split(img_array, dataModel, random_state = 0, test_size = 0.1)
+    X_train, X_test, y_train, y_test = train_test_split(img_array, dataModel, random_state = 0, test_size = 0.15)
+    #print(X_train.shape)
 
     #Build sequential CNN
-    
+    model = Sequential()
+
+    #Configure convolutional layers, specify filter and activation function
+    model.add(Conv2D(16, (3,3), activation='relu', input_shape = X_train[0].shape))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(2,2))
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(32, (3,3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(2,2))
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(2,2))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(128, (3,3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(2,2))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+
+    model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+
+    model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+
+    model.add(Dense(len(data.columns) - 5, activation='sigmoid'))
+    #print(model.summary())
+    #print("train:", X_train.shape)
+    #print("test:", X_test.shape)
+
+    # Compile and fit the model. Validation data is the data used to evaluate the accuracy of our model
+    model.compile(optimizer='adam', loss = 'binary_crossentropy', metrics=['accuracy'])
+    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+    return data, model, history
+
+def plot_learningCurve(history, epoch):
+  # Plot training & validation accuracy values
+  epoch_range = range(1, epoch+1)
+  plt.plot(epoch_range, history.history['accuracy'])
+  plt.plot(epoch_range, history.history['val_accuracy'])
+  plt.title('Model accuracy')
+  plt.ylabel('Accuracy')
+  plt.xlabel('Epoch')
+  plt.legend(['Train', 'Val'], loc='upper left')
+  plt.show()
+
+  # Plot training & validation loss values
+  plt.plot(epoch_range, history.history['loss'])
+  plt.plot(epoch_range, history.history['val_loss'])
+  plt.title('Model loss')
+  plt.ylabel('Loss')
+  plt.xlabel('Epoch')
+  plt.legend(['Train', 'Val'], loc='upper left')
+  plt.show()
+
+
+def predictGenre(img):
+    img = image.load_img(img, target_size = (img_width, img_height, 3))
+    plt.imshow(img)
+    img = image.img_to_array(img)
+    img = img/255.0
+    img = img.reshape(1, img_width, img_height, 3)
+
+    genres = data.columns[5:]
+    print(genres)
+    probGenres = model.predict(img)
+    print(probGenres[0])
+    print(np.argsort(probGenres[0]))
+    top3Genres = np.argsort(probGenres[0])[:-4:-1]
+    print(probGenres[0][top3Genres])
+
+    for i in range(3):
+        if top3Genres[i] > 0.5:
+            print(genres[top3Genres[i]])
+    return genres[top3Genres]
 
 
     
-setupNeuralNetwork()
-processImages("posters")
+data, model, history = setupNeuralNetwork()
+
+sampleImg = (folder + "/movie") + str(5) + ".png"
+plot_learningCurve(history, 5)
+predictGenre(sampleImg)
